2f472bcc843a0ed9cdd66ea1591f7eecde2150d4
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-07-12 22:49:18 +0400

    First progress.

--- PartUtilities/JSIPartComponentToggle.cs
+++ PartUtilities/JSIPartComponentToggle.cs
@@ -1,10 +1,47 @@
 ﻿using System;
+using UnityEngine;
 
-namespace PartUtilities
+namespace JSIPartUtilities
 {
 	public class JSIPartComponentToggle: PartModule
 	{
 
+		[KSPField]
+		public string componentName = string.Empty;
+		[KSPField (isPersistant = true)]
+		public bool componentIsEnabled = true;
+		[KSPField]
+		public bool activeInEditor = true;
+		[KSPField]
+		public bool activeInFlight = true;
+
+		public override void OnStart (PartModule.StartState state)
+		{
+			Component thatComponent = part.FindModelComponent<Component> (componentName);
+			if (thatComponent == null) {
+				JUtil.LogErrorMessage (this, "Target part component {0} was not found. Selfdestructing the module...");
+				Destroy (this);
+			} else {
+				ToggleState (part, componentName, isEnabled);
+			}
+		}
+
+		[KSPEvent (guiActive = true, guiActiveEditor = true, guiName = "Toggle component")]
+		public void GuiToggleComponent ()
+		{
+			componentIsEnabled = !componentIsEnabled;
+			ToggleState (part, componentName, componentIsEnabled);
+		}
+
+		private static void ToggleState (Part thatPart, string targetName, bool state)
+		{
+			Component thatComponent = thatPart.FindModelComponent<Component> (targetName);
+			foreach (Renderer thatRenderer in thatComponent.GetComponentsInChildren<Renderer>()) {
+				thatRenderer.enabled = state;
+			}
+			foreach (Collider thatCollider in thatComponent.GetComponentsInChildren<Collider>()) {
+				thatCollider.enabled = state;
+			}
+		}
 	}
 }
-
---
+++ PartUtilities/UtilityFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace JSIPartUtilities
+{
+	public static class JUtil
+	{
+		public static bool debugLoggingEnabled = true;
+
+		public static void LogMessage (object caller, string line, params object[] list)
+		{
+			if (debugLoggingEnabled)
+				Debug.Log (String.Format (caller.GetType ().Name + ": " + line, list));
+		}
+
+		public static void LogErrorMessage (object caller, string line, params object[] list)
+		{
+			Debug.LogError (String.Format (caller.GetType ().Name + ": " + line, list));
+		}
+	}
+}
+
