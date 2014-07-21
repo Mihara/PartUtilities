d66a1dc6e2454411233d70705734e8094e45aa47
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-07-21 23:38:50 +0400

    ...I'm not sure why you would want to do this but why not?

---
+++ PartUtilities/JSIForbidIVAHere.cs
@@ -0,0 +1,26 @@
+﻿
+namespace JSIPartUtilities
+{
+	public class JSIForbidIVAHere: PartModule
+	{
+		// This needs to be done in this particular order.
+		// Early in the update cycle, we check if we are not the active vessel and return IVAability if we aren't.
+		public void Update ()
+		{
+			if (HighLogic.LoadedSceneIsFlight && !vessel.isActiveVessel) {
+				HighLogic.CurrentGame.Parameters.Flight.CanIVA = true;
+			}
+		}
+
+		// But late in the update cycle, if we are the active vessel, we take it away.
+		// This way, multiple pods with this module should not interfere with each other
+		// or prevent other pods from IVAing.
+		public void LateUpdate ()
+		{
+			if (HighLogic.LoadedSceneIsFlight && vessel.isActiveVessel) {
+				HighLogic.CurrentGame.Parameters.Flight.CanIVA = false;
+			}
+		}
+	}
+}
+
