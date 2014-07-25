851e00b5038b84d99950b1e96ca836193e2f64e1
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-07-25 12:41:46 +0400

    That way is better.

--- PartUtilities/Actuator.cs
+++ PartUtilities/Actuator.cs
@@ -11,7 +11,7 @@ namespace JSIPartUtilities
 		PartModule,
 		TransformTexture,
 		TransformShader,
-		MaxTemp
+		StraightParameter,
 	}
 
 	public class Actuator
@@ -23,8 +23,19 @@ namespace JSIPartUtilities
 		private readonly Transform targetTransform;
 		private readonly string textureLayer = "_MainTex";
 		private readonly string falseString, trueString;
-		private readonly float originalMaxTemp;
-		private readonly float enabledMaxTemp;
+		private readonly float originalParameterValue;
+		private readonly float addToParameterWhenEnabled;
+		private readonly string nameOfParameter = string.Empty;
+
+		private string[] knownStraightParameters = {
+			"mass",
+			"maxTemp",
+			"crashTolerance",
+			"maximum_drag",
+			"minimum_drag",
+			"breakingForce",
+			"breakingTorque"
+		};
 
 		public Actuator (string configData, ActuatorType creatingType, Part thatPart)
 		{
@@ -96,14 +107,69 @@ namespace JSIPartUtilities
 					throw new ArgumentException ("Bad arguments.");
 				}
 				break;
-			case ActuatorType.MaxTemp:
-				if (tokens.Length == 2) {
-					if (float.TryParse (tokens [1], out enabledMaxTemp)) {
-						originalMaxTemp = thatPart.maxTemp;
+			case ActuatorType.StraightParameter:
+				if (tokens.Length == 3) {
+					if (float.TryParse (tokens [1], out addToParameterWhenEnabled)) {
+						if (Array.IndexOf (knownStraightParameters, tokens [0].Trim ()) >= 0) {
+							nameOfParameter = tokens [0].Trim ();
+							originalParameterValue = GetParameter (nameOfParameter, thatPart);
+						} else {
+							throw new ArgumentException ("Bad arguments, unknown straight parameter " + tokens [0]);
+						}
 					} else {
 						throw new ArgumentException ("Bad argument, maxTemp must be a float.");
 					}
+				} else {
+					throw new ArgumentException ("Bad arguments.");
 				}
+				break;
+			}
+		}
+
+		private static float GetParameter (string name, Part thatPart)
+		{
+			switch (name) {
+			case "mass":
+				return thatPart.mass;
+			case "maxTemp":
+				return thatPart.maxTemp;
+			case "crashTolerance":
+				return thatPart.crashTolerance;
+			case "maximum_drag":
+				return thatPart.maximum_drag;
+			case "minimum_drag":
+				return thatPart.minimum_drag;
+			case "breakingForce":
+				return thatPart.breakingForce;
+			case "breakingTorque":
+				return thatPart.breakingTorque;
+			}
+			return 0;
+		}
+
+		private static void SetParameter (string name, Part thatPart, float value)
+		{
+			switch (name) {
+			case "mass":
+				thatPart.mass = value;
+				break;
+			case "maxTemp":
+				thatPart.maxTemp = value;
+				break;
+			case "crashTolerance":
+				thatPart.crashTolerance = value;
+				break;
+			case "maximum_drag":
+				thatPart.maximum_drag = value;
+				break;
+			case "minimum_drag":
+				thatPart.minimum_drag = value;
+				break;
+			case "breakingForce":
+				thatPart.breakingForce = value;
+				break;
+			case "breakingTorque":
+				thatPart.breakingTorque = value;
 				break;
 			}
 		}
@@ -134,8 +200,12 @@ namespace JSIPartUtilities
 				Renderer shm = targetTransform.GetComponent<Renderer> ();
 				shm.material.shader = Shader.Find (newstate ? trueString : falseString);
 				break;
-			case ActuatorType.MaxTemp:
-				thatPart.maxTemp = newstate ? enabledMaxTemp : originalMaxTemp;
+			case ActuatorType.StraightParameter:
+				if (newstate) {
+					SetParameter (nameOfParameter, thatPart, originalParameterValue + addToParameterWhenEnabled);
+				} else {
+					SetParameter (nameOfParameter, thatPart, originalParameterValue);
+				}
 				break;
 			}
 		}
