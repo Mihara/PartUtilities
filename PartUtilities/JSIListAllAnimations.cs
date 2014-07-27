b20ae3a254efd7d4827295559543c6d0e6c70410
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-07-27 18:58:01 +0400

    Handy little idea.

---
+++ PartUtilities/JSIListAllAnimations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace JSIPartUtilities
+{
+	public class JSIListAllAnimations:PartModule
+	{
+
+		// With special thanks to ozraven for his idea.
+		public override void OnStart (StartState state)
+		{
+			if (state == StartState.Editor) {
+				var sb = new StringBuilder ();
+				sb.Append (string.Format ("Animation names in part '{0}':\n", part.partName));
+
+				var names = new List<string> ();
+				foreach (Animation thatAnim in part.FindModelAnimators ()) {
+					names.Add (thatAnim.name);
+				}
+				sb.Append (string.Join (", ", names.ToArray ()));
+				JUtil.LogMessage (this, sb.ToString ());
+			}
+		}
+	}
+}
+
